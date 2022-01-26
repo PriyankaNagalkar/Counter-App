798231eae11e857ec3c1504c0c9aad2c6f90162b
Priyanka Rajesh Nagalkar <93524881+PriyankaNagalkar@users.noreply.github.com>
2022-01-26 17:44:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,57 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
   const [message, setMessage] = useState("");
+  const [disable, setDisable] = useState(false);
+  const [disabled1, setDisabled1] = useState(false);
 
   const increment = () => {
-    setCount(count + 1 >= 100 ? 100 : count + 1);
-    setMessage(count >= 100 ? "Value can't greater than 100" : `Your current value of count is ${count}`);
+    setCount(count + 1 >= 10 ? 10 : count + 1);
+    setMessage(
+      count >= 10
+        ? "Value can't greater than "
+        : `Your current value of count is  `
+    );
+    if (count + 1 >= 10) {
+      setDisable(true);
+      setDisabled1(false);
+    }
   };
 
   const decrement = () => {
     setCount(count <= 0 ? 0 : count - 1);
-    setMessage(count<=0? "Value can't be less than 0":`Your current value of count is ${count}`);
-  }
+    setMessage(
+      count <= 0
+        ? "Value can't be less than "
+        : `Your current value of count is `
+    );
+    if (count - 1 <= 0) {
+      setDisabled1(true);
+    }
+    if (count - 1 <= 10) {
+      setDisable(false);
+    }
+  };
 
   const reset = () => {
     setCount(0);
-    setMessage("")
-  }
+    setMessage("");
+  };
 
-return (
-      <div className="container">
+  return (
+    <div className="container">
       <h1 className="heading">Welcome to My Counter</h1>
       <div className="App">
-      {/* <h1>Hello</h1> */}
-      <header>
-        {/* <h1 className="heading">Welcome to My Counter</h1> */}
-      </header>
-      <h2>{message}</h2>
-      <button onClick={increment}>Increase Value</button>
-      <button onClick={decrement}>Decrease Value</button>
-      <button onClick={reset}>Reset Value</button>
+        <h2>
+          {message} {count}
+        </h2>
+        <button onClick={increment}>Increase Value</button>
+        <button onClick={decrement}>Decrease Value</button>
+        <button onClick={reset}>Reset Value</button>
       </div>
-      </div>
+    </div>
   );
 }
 
